66053c2171465b9a07de8c813480e6571382217d
Hazem Kawas <hazem.k.4488gmail.com>
2021-11-12 13:55:25 +0100

    fixing zoom-in

--- main.js
+++ main.js
@@ -9,9 +9,12 @@ function main() {
     toggleMenu();
 }
 
-function toggleMenu(x) {
+function toggleMenu() {
+    
     const selectElement = (element) => document.querySelector(element);
     selectElement('.mobile-burger-container').addEventListener('click', () => {
         selectElement('header').classList.toggle('active');
     });
+
+    
 }
